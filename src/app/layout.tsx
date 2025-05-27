e239a653169ddd3c370cdd8042e6901da206031a
DevPauloV <vitorcanalle@gmail.com>
2025-05-27 16:57:44 -0300

    feat: update dependencies and add new UI components
    - Added Radix UI components: Avatar, Dialog, Dropdown Menu, Tooltip, and Sheet.
    - Introduced new utility functions for class name merging.
    - Updated global styles with new CSS variables and Tailwind CSS imports.
    - Refactored layout and page components to integrate new UI elements.
    - Enhanced Sidebar component with Sheet for navigation.
    - Improved button styling and added variants for better UI consistency.
    - Cleaned up unused imports and optimized existing components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter} from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Sidebar } from "@/components/ui/sidebar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={cn(
+          "min-h-screen bg-white text-slate-900 antialiased",
+          inter.className
+        )}>
+        <Sidebar /> 
         {children}
       </body>
     </html>
