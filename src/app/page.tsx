9eb93cfb6c84ab5c7ca8e17594c5a8b1aad5e1dd
DevPauloV <vitorcanalle@gmail.com>
2025-05-28 08:42:20 -0300

    feat: enhance Home component layout with additional CardDescription elements and improve responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,70 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import { DollarSign, Percent, Users } from "lucide-react";
 
 
 export default function Home() {
   return (
     <main className="sm:ml-14 p-4">
-      <section className="grid grid-cols-2 gap-4">
+      <section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
         <Card>
           <CardHeader>
-            <div>
-              <CardTitle>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
                 Total vendas
               </CardTitle>
+              <DollarSign className="ml-auto w-4 h-4"/>
             </div>
+
+            <CardDescription>
+              Total vendas em 90 dias
+            </CardDescription>
           </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">R$ 40.000</p>
+          </CardContent>
         </Card>
+
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Novos clientes
+              </CardTitle>
+              <Users className="ml-auto w-4 h-4"/>
+            </div>
+
+            <CardDescription>
+              Novos clientes em 30 dias
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">234</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Pedidos hoje
+              </CardTitle>
+              <Percent className="ml-auto w-4 h-4"/>
+            </div>
+
+            <CardDescription>
+              Total de pedidos feitos hoje
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">65</p>
+          </CardContent>
+        </Card>
+
+
 
 
       </section>
